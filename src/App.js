0cf9c1b0680f0e03f654e51914d1824d85aa1c02
proiacm <proiacm@gmail.com>
2020-10-25 16:29:58 -0600

    Change App to class

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
-
+import React, { Component } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      Glamlex
-    </div>
-  );
+class App extends Component {
+
+  render() {
+    return (
+      <div className="App">
+        Glamlex
+      </div>
+    );
+  }
 }
 
 export default App;
