6109ac0b8d78df5c6c1d5a5e8acf55395e80ca04
proiacm <proiacm@gmail.com>
2020-12-01 12:49:01 -0700

    Update Nav

--- src/Nav/Nav.js
+++ src/Nav/Nav.js
@@ -6,8 +6,10 @@ const Nav = () => {
 
     return (
         <div className="sidenav">
+        <h2>Glamlex <br/>by Alex Lamonte</h2>  
         <Link style={{ cursor: "pointer"}} to="about" spy={true} smooth={true}><u>About</u></Link>
         <Link style={{ cursor: "pointer"}} to="offer" spy={true} smooth={true}><u>Services</u></Link>
+        <Link style={{ cursor: "pointer"}} to="gallery" spy={true} smooth={true}><u>Gallery</u></Link>
         <Link style={{ cursor: "pointer"}} to="appointment" spy={true} smooth={true}><u>Appointments</u></Link>
       </div>
     )
