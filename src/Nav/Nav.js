f44058398c88b07df38dfe2a6378ba0758adbfa2
proiacm <proiacm@gmail.com>
2020-12-09 20:37:25 -0700

    Restyle Nav

--- src/Nav/Nav.js
+++ src/Nav/Nav.js
@@ -5,8 +5,8 @@ import {Link} from 'react-scroll';
 const Nav = () => {
 
     return (
-        <div className="sidenav">
-        <h2 className='title'>Glamlex <br/>by Alex Lamonte</h2>  
+        <div className="nav">
+        {/* <h2 className='title'>Glamlex <br/>by Alex Lamonte</h2>   */}
         <Link style={{ cursor: "pointer"}} to="about" spy={true} smooth={true}><u>About</u></Link>
         <Link style={{ cursor: "pointer"}} to="offer" spy={true} smooth={true}><u>Services</u></Link>
         <Link style={{ cursor: "pointer"}} to="gallery" spy={true} smooth={true}><u>Gallery</u></Link>
