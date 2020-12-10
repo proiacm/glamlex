0758d930474d562ccea5c5992ecc0f682ff94c97
proiacm <proiacm@gmail.com>
2020-12-09 20:37:31 -0700

    Restyle Offer

--- src/Offer/Offer.js
+++ src/Offer/Offer.js
@@ -6,7 +6,7 @@ const Offer = (props) => {
     return (
             <div className='offer' id='offer'>
                <h2><u>{props.name}</u></h2>
-              <p className='services-text'>{props.desc}</p> ...<br/><br/><br/>
+              <p className='services-text'>{props.desc}</p> <br/><br/><br/><br/><br/><br/><br/><br/><br/><br/>
               <p>{props.price}</p>
             </div>   
     )
