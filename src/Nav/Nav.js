e282cf19e3740d7b8216f2ca2a48bb8fc9f04a26
proiacm <proiacm@gmail.com>
2020-12-22 13:00:24 -0700

    Introduce Sidenav instead of top nav

--- src/Nav/Nav.js
+++ src/Nav/Nav.js
@@ -8,11 +8,9 @@ const Nav = () => {
     return (
         <div className="nav">
         <img className='nav-img' src={logo} alt='logo' />
-        <p className='nav-li'> <Link style={{ cursor: "pointer"}} to="appointment" spy={true} smooth={true}>Appointments</Link> </p>
+        {/* <p className='nav-li'> <Link style={{ cursor: "pointer"}} to="appointment" spy={true} smooth={true}>Appointments</Link> </p>
         <p className='nav-li'> <Link style={{ cursor: "pointer"}} to="gallery" spy={true} smooth={true}>Gallery</Link> </p>
-        <p className='nav-li'> <Link style={{ cursor: "pointer"}} to="offer" spy={true} smooth={true}>Services</Link> </p>
-        <p className='nav-li'> <Link style={{ cursor: "pointer"}} to="about" spy={true} smooth={true}>About</Link> </p>
-        
+        <p className='nav-li'> <Link style={{ cursor: "pointer"}} to="offer" spy={true} smooth={true}>Services</Link> </p>         */}
 
       </div>
     )
